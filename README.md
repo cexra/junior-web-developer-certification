e620294ed4bdb24c6b2b2c16cab36a16638b3fd6
vexra <unique123wowros@gmail.com>
2023-08-26 20:30:25 +0700

    final

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# Navatel
+
+Web Aplication for room reservation in Navatel
+
+
+## Authors
+
+- [@cexra](https://www.github.com/cexra)
+- [@vexra](https://www.github.com/vexra)
+
+
+## Installation
+
+1. Copy entire folder to server folder e.g. htdocs
+2. Import hotel.sql
+3. Run Apache Web Server, MySql,
+4. Go to localhost/junior-web-developer/public
+    
+## Tech Stack
+
+**Client:** TailwindCSS
+
+**Server:** lampp or xampp
+
---
+++ public/docs/js/searchIndex.js
@@ -0,0 +1,19 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\hitung_jumlah\u0028\u0029",
+            "name": "hitung_jumlah",
+            "summary": "Calculates\u0020the\u0020sum\u0020of\u0020numbers\u0020from\u00201\u0020to\u0020n\u0020using\u0020the\u0020formula\u0020n\u002A\u0028n\u002B1\u0029\/2.",
+            "url": "namespaces/default.html#function_hitung_jumlah"
+        },                {
+            "fqsen": "\\hitung_jumlah2\u0028\u0029",
+            "name": "hitung_jumlah2",
+            "summary": "Calculates\u0020the\u0020sum\u0020of\u0020numbers\u0020from\u00201\u0020to\u0020n\u0020using\u0020a\u0020loop.",
+            "url": "namespaces/default.html#function_hitung_jumlah2"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
